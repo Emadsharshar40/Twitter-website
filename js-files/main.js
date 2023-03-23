fb9b5cfb55ee66655a8cf58a1162c2312456a57c
Emad sharshar <128221405+Emadsharshar40@users.noreply.github.com>
2023-03-23 15:30:16 +0200

    Add files via upload

---
+++ js-files/main.js
@@ -0,0 +1,11 @@
+
+$('.js').click(function() {
+   $('header').toggleClass("show");
+   $('.js').toggleClass("move");
+   $('article .header-content i').toggleClass("color");
+});
+$('.js2').click(function() {
+   $('section').toggleClass("show2");
+   $('.js2').toggleClass("rotate");
+   $('article .header-content i').toggleClass("color");
+});
